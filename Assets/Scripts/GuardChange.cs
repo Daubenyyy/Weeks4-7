1260b5b4b33b0509ad8a466584a9afa91c0c14a0
JackM <mitcjack@sheridancollege.ca>
2025-02-17 19:36:46 -0500

    completed functioning animation for guard change slider

--- Assets/Scripts/GuardChange.cs
+++ Assets/Scripts/GuardChange.cs
@@ -6,15 +6,34 @@ public class GuardChange : MonoBehaviour
 {
     public float guard;
 
+    private Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        changeGuard();
+    }
+
+    void changeGuard()
+    {
+        if (guard == 0) animator.SetBool("HighGuard", true);
+        else animator.SetBool("HighGuard", false);
+
+        if (guard == 1) animator.SetBool("MidGuard", true);
+        else animator.SetBool("MidGuard", false);
+
+        if (guard == 2) animator.SetBool("LowGuard", true);
+        else animator.SetBool("LowGuard", false);
+    }
+
+    public void guardSlider(float g)
+    {
+        guard = g;
     }
 }
